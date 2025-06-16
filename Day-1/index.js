5ef097e119b1282a71247d8a1f0302fda9e9cef8
Ahsan Sheikh <155547915+AhsanSdev@users.noreply.github.com>
2025-06-16 22:58:50 +0500

    a way to prompt

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,9 +1,17 @@
-// console.log("Hello World");
+// // console.log("Hello World");
 
-const i = 30;
+// const i = 30;
 
-if (i == 30) {
-  console.log("i is 30");
-} else {
-  console.log("i is not 30");
+// if (i == 30) {
+//   console.log("i is 30");
+// } else {
+//   console.log("i is not 30");
+// }
+
+
+const age = window.prompt("Enter your age: ");
+if (age >= 18) {
+  console.log("You are an adult.");
+}else {
+  console.log("You are a minnow.");
 }
