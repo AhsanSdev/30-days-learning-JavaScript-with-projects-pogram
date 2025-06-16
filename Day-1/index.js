156ab86855c76a85077065ee75fe9c2ce8297f74
Ahsan Sheikh <155547915+AhsanSdev@users.noreply.github.com>
2025-06-16 23:04:57 +0500

    today done

--- Day-1/index.js
+++ Day-1/index.js
@@ -9,9 +9,21 @@
 // }
 
 
+// const age = window.prompt("Enter your age: ");
+// if (age >= 18) {
+//   console.log("You are an adult.");
+// }else {
+//   console.log("You are a minnow.");
+// }
+
 const age = window.prompt("Enter your age: ");
-if (age >= 18) {
-  console.log("You are an adult.");
-}else {
-  console.log("You are a minnow.");
+
+function checkAge(age) {
+  if (age >= 18) {
+    document.getElementById("age").innerHTML = "You are an adult.";
+  } 
+  
+  else {
+    document.getElementById("age").innerHTML = "You are a minor.";
+  }
 }
