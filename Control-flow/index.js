f71a797ea9a07624a3b197401cf941472491db73
Ahsan Sheikh <155547915+AhsanSdev@users.noreply.github.com>
2025-06-17 15:40:25 +0500

    logic one done

--- Control-flow/index.js
+++ Control-flow/index.js
@@ -4,29 +4,52 @@
 // codes:- A=65, Z=90, a=97, z=122)  
 
 
-let input = window.prompt("Enter a single character:");
+// let input = window.prompt("Enter a single character:");
 
-if (input && input.length === 1) {
-    let ASCII = input.charCodeAt(0);
+// if (input && input.length === 1) {
+//     let ASCII = input.charCodeAt(0);
 
 
-if (ASCII >= 48 && ASCII <= 57) {
-    console.log("it's a number");        
+// if (ASCII >= 48 && ASCII <= 57) {
+//     console.log("it's a number");        
+// }
+
+// else if (ASCII >= 65 && ASCII <= 90) {
+//     console.log("it's a UPPERCASE letter");
+// }
+
+// else if(ASCII >= 97 && ASCII <= 122) {
+//     console.log("it's a lowercase letter");
+// }
+
+// else {
+//     console.log("it's a special character");    
+// }
+// }
+
+// else {
+//     console.log("please enter a valid character");    
+// }
+
+
+
+
+
+// Write a JavaScript program that accept two integers and 
+// display the larger. Also show if the two integers are equal.
+
+
+// My logic one
+
+//parseInt is a built in function converts a string to an integer
+
+let num1 = parseInt(window.prompt("enter a number"))
+let num2 = parseInt(window.prompt("enter a number"))
+
+if (num1 > num2) {
+    console.log(`${num1} "is greater than" ${num2}`);
 }
 
-else if (ASCII >= 65 && ASCII <= 90) {
-    console.log("it's a UPPERCASE letter");
-}
-
-else if(ASCII >= 97 && ASCII <= 122) {
-    console.log("it's a lowercase letter");
-}
-
-else {
-    console.log("it's a special character");    
-}
-}
-
-else {
-    console.log("please enter a valid character");    
+else if (num2 > num1) {
+    console.log(`${num2} "is greater than" ${num1}`);
 }
