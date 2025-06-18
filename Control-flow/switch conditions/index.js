827f02d07d2f72d276816849f5cd484813008b91
Ahsan Sheikh <155547915+AhsanSdev@users.noreply.github.com>
2025-06-18 13:51:20 +0500

    second example done

--- Control-flow/switch conditions/index.js
+++ Control-flow/switch conditions/index.js
@@ -1,32 +1,59 @@
-let color = window.prompt("please enter your favaroite background color");
+// let color = window.prompt("please enter your favaroite background color");
 
-switch (color) {
+// switch (color) {
+//     case "red":
+//         window.alert("Your background color is changed to red");
+//         document.body.style.backgroundColor = color;
+//     break;
+
+//     case "green":
+//         window.alert("Your background color is changed to green");
+//         document.body.style.backgroundColor = color;
+//     break;
+
+//     case "blue":
+//         window.alert("Your background color is changed to blue");
+//         document.body.style.backgroundColor = color;
+//     break;
+
+//     case "purple":
+//         window.alert("Your background color is changed to purple");
+//         document.body.style.backgroundColor = color;
+//     break;
+
+//     case "black":
+//         window.alert("Your background color is changed to red");
+//         document.body.style.backgroundColor = color;
+//     break;
+
+//     default:
+//         window.alert("please choose a valid bg color");
+//         break;
+// }
+
+
+// Prompt user to enter a traffic light color: "red", "yellow", "green"
+// Show alert with instruction:
+// "Stop" for red
+// "Ready" for yellow
+// "Go" for green
+
+let input = window.prompt("please enter your traffic light color").toLowerCase();
+
+switch (input) {
     case "red":
-        window.alert("Your background color is changed to red");
-        document.body.style.backgroundColor = color;
-    break;
+        window.alert("Stop");
+        break;
+
+    case "yellow":
+        window.alert("Ready");
+        break;
 
     case "green":
-        window.alert("Your background color is changed to green");
-        document.body.style.backgroundColor = color;
-    break;
-
-    case "blue":
-        window.alert("Your background color is changed to blue");
-        document.body.style.backgroundColor = color;
-    break;
-
-    case "purple":
-        window.alert("Your background color is changed to purple");
-        document.body.style.backgroundColor = color;
-    break;
-
-    case "black":
-        window.alert("Your background color is changed to red");
-        document.body.style.backgroundColor = color;
-    break;
+        window.alert("Go");
+        break;
 
     default:
-        window.alert("please choose a valid bg color");
+        window.alert("please enter a valid traffic light color");
         break;
 }
