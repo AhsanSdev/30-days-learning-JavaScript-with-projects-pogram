dd7e961a85d63aaed8da5f2e002b0b85088b547c
Ahsan Sheikh <155547915+AhsanSdev@users.noreply.github.com>
2025-06-29 22:55:13 +0500

    second question done

--- Arrays Methods/index.js
+++ Arrays Methods/index.js
@@ -1,8 +1,16 @@
 // Declare an empty array using JS literal notation to store 
 // student names in future.
 
-//Literal notation = direct likhna, bina kisi function ke
+//Literal notation = [] ka use karke direct likhna, bina kisi function ke
 
 
-let studentName = [];
+// let studentName = [];
 
+//Declare an empty array using JS object notation to store 
+//student names in future
+
+//Object notation = {} ka use karke object ko direct banana.
+
+// let data = {
+//     studentName : []
+// }
