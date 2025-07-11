50467ef6c9d68ae47ebe2878f29aa06a57b7d564
Ahsan Sheikh <155547915+AhsanSdev@users.noreply.github.com>
2025-07-11 16:01:06 +0500

    done

---
+++ Counter_app/index.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+//Use ++count for update first, then show it
+//++count ka faida === Pehle increment hota hai, phir value return hoti hai jabke normal cound me Pehle value return hoti hai, phir increment hota hai
+
+function add(){
+    document.getElementById("counter").innerHTML = ++count;
+}
+
+//my biggest mistake i didnt put qoutes in counter
+//function callback html file me ho raha hai yahan nahi
+
+function minus(){
+    document.getElementById("counter").innerHTML = --count;
+}
+
+
+function reset(){
+    document.getElementById("counter").innerHTML = 0;
+}
